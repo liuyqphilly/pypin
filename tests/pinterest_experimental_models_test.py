8f3449d619773c2dc63b5b10715c8c78af9d491a
@be <abeahmed2@gmail.com>
2017-04-12 15:23:11 -0400

    Tests for the follower/following classes.

--- tests/pinterest_experimental_models_test.py
+++ tests/pinterest_experimental_models_test.py
@@ -43,5 +43,45 @@ class TestPinsV3Model(unittest.TestCase):
     def test_board_pins_id_getter(self):
         self.assertIsNotNone(self.pin.id, msg='pin id should be initialized.')
 
+class TestUserFollowersV3Model(unittest.TestCase):
+
+    def setUp(self):
+        # TODO: Don't use a live pin request between each test, store in cassete and make sure it
+        # matches the one that comes back from the api in another test so we only make one request.
+        self.api = pypin.API(os.environ['PIN_TOKEN'], os.environ['PIN_V3_TOKEN'])
+        self.followers = self.api.get_user_followers_v3('hawkins6654')
+
+    def test_constructor(self):
+        # TODO: Remove the hardcoded sample Pin and use some sort of cassettes.
+        self.assertIsInstance(self.followers, pypin.UserFollowersV3, msg='pin should be type pypin.models.UserFollowersV3')
+
+    def test_status(self):
+        self.assertEqual(self.followers.get('status'), "success", msg='status should be "success".')
+
+    def test_paginated(self):
+        for follower in self.followers:
+            pass
+            # print(follower)
+
+class TestUserFollowingV3Model(unittest.TestCase):
+
+    def setUp(self):
+        # TODO: Don't use a live pin request between each test, store in cassete and make sure it
+        # matches the one that comes back from the api in another test so we only make one request.
+        self.api = pypin.API(os.environ['PIN_TOKEN'], os.environ['PIN_V3_TOKEN'])
+        self.following = self.api.get_user_following_v3('hawkins6654')
+
+    def test_constructor(self):
+        # TODO: Remove the hardcoded sample Pin and use some sort of cassettes.
+        self.assertIsInstance(self.following, pypin.UserFollowingV3, msg='pin should be type pypin.models.UserFollowersV3')
+
+    def test_status(self):
+        self.assertEqual(self.following.get('status'), "success", msg='status should be "success".')
+
+    def test_paginated(self):
+        for user in self.following:
+            pass
+            # print(follower)
+
 if __name__ == '__main__':
     unittest.main()
